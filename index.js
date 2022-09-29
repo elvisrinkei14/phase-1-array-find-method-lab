2384e6d51803f06328825d830eacea5b5441cd7b
Moringa School <elvis.rinkei@moringaschool.com>
2022-09-29 12:26:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+let x ;
+function superbowlWin(record){
+    x = record.find(({result}) => result === 'W');
+    if (x === undefined){
+        return;
+    }
+    else if(x.year === '2015'){
+        return x.year;
+    }
+}
